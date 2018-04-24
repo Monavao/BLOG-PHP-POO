fd5efb37b3ac1fa80abdd53829e1c1a5c6a99284
Monavao <omid.monavari@hotmail.fr>
2018-04-24 18:43:24 +0200

    Test - Utilisation d'une façade

--- apps/Controller/DemoController.php
+++ apps/Controller/DemoController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller;
 
-use Core\Database\QueryBuilder;
+//use Core\Database\QueryBuilder;
 
 class DemoController extends AppController
 {
 	public function index()
 	{
-		$query = new QueryBuilder();
-		echo $query
-			->select('id', 'titre', 'contenu')
+		require ROOT . '/Query.php';
+
+		//$query = new QueryBuilder();
+
+		echo \Query::select('id', 'titre', 'contenu')
 			->from('articles', 'Post')
 			->where('Post.categorie_id = 1')
-			->where('Post.date > NOW()'); // si on utilise __toString() dans class QueryBuikder
-			//->getQuery(); // si on utilise getQuery() dans class QueryBuikder
+			->where('Post.date > NOW()'); // si on utilise __toString() dans class QueryBuilder
+			//->getQuery(); // si on utilise getQuery() dans class QueryBuilder
 	}
 }
